ac382852889358eda41359894a82908df916da10
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2014-03-04 22:41:48 +0100

    add functions

--- src/selearea.py
+++ src/selearea.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+def get_ast(url = None, webelement = None):
+	"""
+	Return an AST for one webpage.
+
+	Each AST stores data about children, size of an element (width, heigth, x, y)
+	"""
+
+	pass
+
+def get_workarea(urls = None, webelements = None):
+	"""
+	Analyze a given webpage and return xpath to a work area.
+
+	:param urls: list with urls
+	:type urls: list
+	:param webelements: list of webelements (from selenium)
+	:type driver: list
+	:return: xpath of work area
+	"""
+
+	pass
