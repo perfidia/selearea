608f7d5f260f97791cc666566fdedb401f069510
inf100364 <kmieciak.tomasz@gmail.com>
2014-03-29 15:03:13 +0100

    Fixing broken commit

--- src/selearea.py
+++ src/selearea.py
@@ -1,97 +1,159 @@
-#-*- coding: utf-8 -*-
-'''
-Created on 23 mar 2014
-
-@author: mariusz
-@author: tomasz
-'''
-import unittest
-import sys
-sys.path.append("../src")
-from selearea import get_ast
-from selearea import get_workarea
-
-
-class seleareaTest(unittest.TestCase):
-
-    ast_list = None
-
-    def get_fc_pages(self):
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+import hashlib
+from selenium import webdriver
+  
+class DOMElement(object):
+    '''
+    Representation of a single node.
+    '''
+    width = None
+    height = None
+    md5 = None
+    x = None
+    y = None
+    xpath = None
+    parent = None
+    children = None
+       
+    def __init__(self, width=None, height=None,
+                    md5=None, x=None, y=None, xpath=None, parent=None, children=None):
+        self.width = width
+        self.height = height
+        self.md5 = md5
+        self.x = x
+        self.y = y
+        self.xpath = xpath
+        self.parent = parent
+        self.children = children
+    
+       
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
         
-        webpages = {"http://fc.put.poznan.pl", 
-        "http://fc.put.poznan.pl/rekrutacja/post-powanie-kwalifikacyjne%2C29.html",
-        "http://fc.put.poznan.pl/o-wydziale/witamy%2C39.html"}
-        
-        self.ast_list = list()
+    def print_all(self, level=1):
+        """
+        Printing content of node.
+        :param level: degree of the current subtree
+        """
+        print ("\t"*level) + self.xpath
+        for child in self.children:
+            child.print_all(level + 1)
+           
+           
+def get_ast(url=None, driver=None):
+    """
+    This function return an ast of webpage. It is a tree that allows comparison of multiple websites.
     
-        for page in webpages:
-            ast = get_ast(page)
-            self.ast_list.append( ast )
-            
-    def get_fce_pages(self):
+    :param url: url of webpage
+    :param driver: selenium driver
+    :return: tree of the webpage
+    """
     
-        webpages = {"http://www.bis.put.poznan.pl/", 
-                "http://www.bis.put.poznan.pl/evPages/show/id/182"}
-        
-        self.ast_list = list()
+    def process_node(html, parent):
+        """
+        Processing single node.
+        :param html: webelement from selenium to process
+        :param parent: parent of given webelement
+        :return: node in a tree
+        """
+        width = html.size['width']
+        height = html.size['height']
+        x = html.location['x']
+        y = html.location['y']
+        md5 = hashlib.md5(html.text.encode('utf-8')).hexdigest()
+        xpath = ""
+           
+        if parent != None:
+            xpath = parent.xpath
+               
+        xpath = xpath + "\\" + str(html.tag_name) + "[@id='" + str(html.get_attribute("id")) + "'][@class='" + str(html.get_attribute("class")) + "']"
+           
+        node = DOMElement(width, height, md5, x, y, xpath, parent, list())
+        children = html.find_elements_by_xpath("child::*")
+       
+        for child in children:
+            node.add_child(process_node(child, node))
+               
+        return node
     
-        for page in webpages:
-            ast = get_ast(page)
-            self.ast_list.append( ast )
+    def check_url(url):
+        """
+        Check if the url is ok.
+        :param url: url of webpage
+        :return: true or false
+        """
+        if not isinstance(url, basestring):
+            raise ValueError("It's not a url.")
 
-    def get_same_pages(self):
-    
-        webpages = {"http://www.bis.put.poznan.pl/", 
-                "http://www.bis.put.poznan.pl/"}
-        
-        self.ast_list = list()
-    
-        for page in webpages:
-            ast = get_ast(page)
-            self.ast_list.append( ast )
+        if (url.startswith("http://")==False) and (url.startswith("https://")==False):
+            raise ValueError("http or https protocol is required")
 
-    def tearDown(self):
-        pass
+        return url
     
+    url = check_url(url)
     
-    def test_get_null_page(self):
-        webpage = "putpoznan.pl"
-        with self.assertRaises(ValueError):
-            get_ast(webpage)
+    if(driver==None):
+        driver = webdriver.Firefox()
         
+    driver.get(url)
+    html = driver.find_elements_by_xpath("child::*")
+    dom = process_node(html[0], None)
+    driver.close()
     
-    def test_get_workarea_identical_pages(self):
-        self.get_same_pages()
-        workarea_list = get_workarea(self.ast_list)
-        self.assertEqual(0, len(workarea_list), "AssertionFailed: workarea found on identical pages.")
-        
+    return dom
+ 
+ 
+ 
+def get_workarea(ast_list):
+    """
+    Analyze a given webpage and return xpath to a work area.
     
-    def test_get_ast_fc_count(self):
+    :param ast_list: list of trees
+    :return: xpaths for elements which are different on the webpages 
+	"""
+    
+    def are_different(node1, node2):
+        """
+        Check if nodes are different (check md5 sum and children of nodes).
+        :param node1: first node to check if nodes are different
+        :param node1: second node to check if nodes are different
+        :return: true or false
+        """
+        different_children = True
+        for child1 in node1.children:
+            for child2 in node2.children:
+                if child1.md5 == child2.md5:
+                    different_children = False
+    
+        return different_children
+
+    
+    def do_analysis(node1, node2):
+        """
+        Analysis of pairs of nodes.
+        :param node1: first node for analysis
+        :param node2: second node for analysis
+        :return list of xpaths
+        """
+        result = list()
          
-        self.get_fc_pages()
-        self.assertEqual(3, len(self.ast_list), "AssertionFailed: count for fc pages.")
-        
-        
-    def test_get_workarea_fc_content(self):
-        
-        self.get_fc_pages()        
-        workarea_list = get_workarea(self.ast_list)
-        xpath = str("\\html[@id=''][@class='js']\\body[@id=''][@class='']\\div[@id='right'][@class='']\\div[@id='content'][@class='']")
-        self.assertEqual(xpath, workarea_list[1], "AssertionFailed: xpaths for fc pages.")
-        
-    def test_get_ast_fce_count(self):
-         
-        self.get_fce_pages()
-        self.assertEqual(2, len(self.ast_list), "AssertionFailed: count for fc pages.")
-        
-        
-    def test_get_workarea_fce_content(self):
-        
-        self.get_fce_pages()        
-        workarea_list = get_workarea(self.ast_list)
-        xpath = str("\\html[@id=''][@class='']\\body[@id=''][@class='']\\div[@id='main'][@class='']\\div[@id=''][@class='']\\div[@id='left_menu'][@class='']\\div[@id='left_menu_box'][@class='']")
-        self.assertEqual(xpath, workarea_list[0], "AssertionFailed: xpaths for fc pages.")
-            
-
-if __name__ == "__main__":
-    unittest.main()
+        for child1 in node1.children:
+            for child2 in node2.children:
+                if child1.xpath == child2.xpath and child1.md5 != child2.md5:
+                    if are_different(child1, child2):
+                        result.append(child1.xpath)
+                    else:
+                        result = result + do_analysis(child1, child2)
+                
+        return result
+    
+    
+    result = list()
+    for i, ast1 in enumerate(ast_list):
+        for j, ast2 in enumerate(ast_list, start=i+1):
+            result = result + do_analysis(ast1, ast2)
+                
+    return list(set(result))
