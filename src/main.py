846097548ddf6255820e9cad9680d03d8c1f0dd5
Bartosz Alchimowicz <bartosz.alchimowicz@cs.put.poznan.pl>
2014-04-03 13:25:01 +0200

    make it prettier prettier

--- src/main.py
+++ src/main.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from selearea import get_ast
-from selearea import get_workarea
 from selenium import webdriver
+from selearea import get_ast, get_workarea
 
 if __name__ == "__main__":
-    
-    webpages = {"http://www.bis.put.poznan.pl/", 
-                "http://www.bis.put.poznan.pl/evPages/show/id/182"}
-        
-    ast_list = list()
+    urls = [
+            "http://www.bis.put.poznan.pl/",
+            "http://www.bis.put.poznan.pl/evPages/show/id/182"
+    ]
 
     driver = webdriver.Firefox()
 
-    for page in webpages:
-        ast = get_ast(page, driver)
-        ast_list.append( ast )
-    
-    for workarea in get_workarea(ast_list):
+    asts = [get_ast(url, driver) for url in urls]
+
+    for workarea in get_workarea(asts):
         print workarea
-        
+
     driver.close()
